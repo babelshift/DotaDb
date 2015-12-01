9775a8d6d13081ae5cea12762406f9b5f77a5424
babelshift <justin.d.skiles@gmail.com>
2015-12-01 08:19:56 -0500

    Initial commit.

---
+++ DotaDb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotaDb.Startup))]
+namespace DotaDb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
