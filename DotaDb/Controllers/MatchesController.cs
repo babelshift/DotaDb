37844b0333e306024c4ba47263f608190dc3d9f3
babelshift <justin.d.skiles@gmail.com>
2015-12-12 11:42:48 -0500

    Added larger minimap image for live league game details page. Starting live league game detail page. Pull some images from CDN instead of public storage URL.

---
+++ DotaDb/Controllers/MatchesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotaDb.Controllers
+{
+    public class MatchesController : Controller
+    {
+        public async Task<ActionResult> Live(long id)
+        {
+            return View();
+        }
+    }
+}
