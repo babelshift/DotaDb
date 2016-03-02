d1c85d9464541a0edbceb66a34163116e4412b84
babelshift <justin.d.skiles@gmail.com>
2016-03-01 21:16:43 -0500

    Added a privacy policy page.

---
+++ DotaDb/Controllers/PrivacyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotaDb.Controllers
+{
+    public class PrivacyController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
