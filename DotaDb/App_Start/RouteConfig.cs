2761f2dfdf3fc90fc81e1345a5a199b2d3c24dfb
babelshift <justin.d.skiles@gmail.com>
2016-03-17 10:34:18 -0400

    Fixed various routing issues. Added robots.txt to stop search engines from indexing live matches.

--- DotaDb/App_Start/RouteConfig.cs
+++ DotaDb/App_Start/RouteConfig.cs
@@ -15,29 +15,29 @@ namespace DotaDb
                 defaults: new { controller = "Heroes", action = "Hero", heroName = UrlParameter.Optional }
             );
 
-            routes.MapRoute(
-                name: "HeroSpecifics",
-                url: "heroes/{id}",
-                defaults: new { controller = "Heroes", action = "Hero" }
-            );
+            //routes.MapRoute(
+            //    name: "HeroSpecifics",
+            //    url: "heroes/{id}",
+            //    defaults: new { controller = "Heroes", action = "Hero" }
+            //);
 
             routes.MapRoute(
                 name: "HeroItemBuildsWithName",
-                url: "heroes/{id}/{heroName}/{action}",
-                defaults: new { controller = "Heroes", heroName = UrlParameter.Optional }
+                url: "heroes/{id}/{heroName}/build",
+                defaults: new { controller = "Heroes", action = "Build", heroName = UrlParameter.Optional }
             );
 
-            routes.MapRoute(
-                name: "HeroItemBuilds",
-                url: "heroes/{id}/{action}",
-                defaults: new { controller = "Heroes" }
-            );
+            //routes.MapRoute(
+            //    name: "HeroItemBuilds",
+            //    url: "heroes/{id}/{action}",
+            //    defaults: new { controller = "Heroes" }
+            //);
 
-            routes.MapRoute(
-                name: "CosmeticItems",
-                url: "items/cosmetics/{prefab}",
-                defaults: new { controller = "Items", Action = "Cosmetics", prefab = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "CosmeticItems",
+            //    url: "items/cosmetics/{prefab}",
+            //    defaults: new { controller = "Items", Action = "Cosmetics", prefab = UrlParameter.Optional }
+            //);
 
             routes.MapRoute(
                 name: "Default",
