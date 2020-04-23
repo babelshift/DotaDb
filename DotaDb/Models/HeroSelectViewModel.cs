3fba2024f0a4aa25a176e2f383112743051dc294
Justin <justin.d.skiles@gmail.com>
2020-04-23 19:52:55 -0400

    Added hero grid page.

---
+++ DotaDb/Models/HeroSelectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DotaDb.Models
+{
+    public class HeroSelectViewModel
+    {
+        public HeroSelectViewModel()
+        {
+            StrengthHeroes = new List<HeroViewModel>();
+            AgilityHeroes = new List<HeroViewModel>();
+            IntelligenceHeroes = new List<HeroViewModel>();
+        }
+
+        public IReadOnlyCollection<HeroViewModel> StrengthHeroes { get; set; }
+        public IReadOnlyCollection<HeroViewModel> AgilityHeroes { get; set; }
+        public IReadOnlyCollection<HeroViewModel> IntelligenceHeroes { get; set; }
+    }
+}
