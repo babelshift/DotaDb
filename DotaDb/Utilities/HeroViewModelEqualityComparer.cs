eb2618be41589686663816891d354468c2e3b915
Justin <justin.d.skiles@gmail.com>
2020-04-27 21:14:07 -0400

    Added basic filtering to Hero table view.

---
+++ DotaDb/Utilities/HeroViewModelEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿using DotaDb.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DotaDb.Utilities
+{
+    public class HeroViewModelEqualityComparer : IEqualityComparer<HeroViewModel>
+    {
+        public bool Equals(HeroViewModel x, HeroViewModel y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(HeroViewModel obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return obj.Id.GetHashCode();
+        }
+    }
+}
